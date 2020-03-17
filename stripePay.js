48fce43add3c27f28e34479c385b06bd864216a1
Gary Pelissier <gpelissier@stripe.com>
2020-03-16 17:50:53 -0700

    Update

--- stripePay.js
+++ stripePay.js
@@ -3,11 +3,25 @@ window.onload = function () {
 
   const buy1 = document.querySelector('#buy1')
   buy1.addEventListener('click', () => {
-console.log("I got clicked")
+
     stripe.redirectToCheckout({
       items: [
         // Replace with the ID of your SKU
         { sku: 'sku_GvIozdfT0tGfEV', quantity: 1 }
+      ],
+      successUrl: 'http://localhost:8000/success.html',
+      cancelUrl: 'http://localhost:8000/cancel.html',
+    }).then(function (result) {
+      console.log(result.error.message)
+    });
+  });
+  const buy2 = document.querySelector('#buy2')
+  buy1.addEventListener('click', () => {
+
+    stripe.redirectToCheckout({
+      items: [
+        // Replace with the ID of your SKU
+        { sku: 'sku_GvJkB1iF05LeOr', quantity: 1 }
       ],
       successUrl: 'http://localhost:8000/success.html',
       cancelUrl: 'http://localhost:8000/cancel.html',
